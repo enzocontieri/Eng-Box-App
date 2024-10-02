88713e90add7703c7fbebca3d6e7cd6f04485590
enzocontieri <enzo.c.contieri@gmail.com>
2024-10-02 10:59:58 -0300

    App Created

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
