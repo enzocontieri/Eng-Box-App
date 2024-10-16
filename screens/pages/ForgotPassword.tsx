3f89f6bb3b3f4532fbe042418686f1f9919a64fe
Hanspeter <hansdietiker2012@gmail.com>
2024-10-16 17:11:09 -0300

    add <Screen> added rnfunction at ForgotPassword

---
+++ screens/pages/ForgotPassword.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ForgotPassword() {
+  return (
+    <View>
+      <Text>ForgotPassword</Text>
+    </View>
+  )
+}
