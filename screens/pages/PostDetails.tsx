26c1485f69d826d1965fdc016a6463082b6cd7ec
Ed Araújo <ejrabt1@gmail.com>
2024-10-20 16:54:06 -0300

    Código da página de Detalhes do Post refatorado para garantir uma melhor responsividade.

--- screens/pages/PostDetails.tsx
+++ screens/pages/PostDetails.tsx
@@ -1,5 +1,5 @@
-import { View, Text, SafeAreaView, StatusBar, Image, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, SafeAreaView, StatusBar, Image, ScrollView, Dimensions } from 'react-native';
+import React, { useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -9,57 +9,55 @@ const PostDetails = () => {
     const navigation = useNavigation();
     const route = useRoute();
 
-    // Estado para o Botão de Curtit
-    const [isLiked, setIsLiked] = useState(false);
-
-    // Parâmetros passados pela navegação
-    const { imageUrl, title, description } = route.params;
+    const [isLiked, setIsLiked] = useState(false); // Estado para o Botão de Curtit
+    const { imageUrl, title, description } = route.params; // Parâmetros passados pela navegação
 
     return (
-        <SafeAreaView  >
-            <TouchableOpacity
-                onPress={() => navigation.goBack()}
-                className='absolute top-10 left-5 bg-white p-2 rounded-full'
-            >
-                <Ionicons name="chevron-back" size={24} color="black" />
-            </TouchableOpacity>
+        <SafeAreaView className='flex-1'>
+            <ScrollView contentContainerStyle={{ paddingBottom: 20 }} showsVerticalScrollIndicator={false}>
 
-            <View className='relative mt-[16] items-center justify-center' >
+                {/* Botão Voltar */}
+                <View className='absolute top-10 left-5 z-10' >
+                    <TouchableOpacity
+                        onPress={() => navigation.goBack()}
+                        className='bg-white p-2 rounded-full shadow-lg '
+                    >
+                        <Ionicons name="chevron-back" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
 
+                {/* Container Principal */}
+                <View className='flex-1 items-center justify-center mt-10' >
 
-                {/* Imagem do Post */}
-                <Image
-                    source={imageUrl}
-                    className='w-full h-[430] mt-[100]'
-                    resizeMode='cover'
-                />
+                    {/* Imagem do Post */}
+                    <Image
+                        source={imageUrl}
+                        className='w-11/12 h-[45vh] rounded-lg mt-16'
+                        resizeMode='cover'
+                    />
 
-                {/* Informações do Post */}
-                <View className='w-[364]' >
-                    {/* Título do Post */}
-                    <Text className='text-[27px] mt-[10]' style={{ fontFamily: 'poppins-medium' }} >{title}</Text>
-                    {/* Descrição do Post */}
-                    <View style={{ maxHeight: 150, marginTop: 10 }}>
-                        <ScrollView showsVerticalScrollIndicator={false} >
-                            <Text className='text-justify mt-[10]' style={{ fontFamily: 'poppins-regular' }}>{description}</Text>
-                        </ScrollView>
-                    </View>
+                    {/* Informações do Post */}
+                    <View className='w-11/12 mt-5' >
+                        {/* Título do Post */}
+                        <Text className='text-2xl mb-2' style={{ fontFamily: 'poppins-medium' }} >{title}</Text>
+                        {/* Descrição do Post */}
+                        <Text className='text-justify mt-2.5 leading-6' style={{ fontFamily: 'poppins-regular' }}>{description}</Text>
 
+                        {/* Botões Curtir + Comentários */}
+                        <View className='flex-row items-center justify-between mt-8' >
+                            {/* Botão Curtir */}
+                            <TouchableOpacity onPress={() => setIsLiked(!isLiked)} className='ml-6' >
+                                <Ionicons name={isLiked ? 'heart' : 'heart-outline'} color={isLiked ? 'red' : '#767676'} size={35} />
+                            </TouchableOpacity>
 
-                    {/* Botões Curtir + Comentários */}
-                    <View className='flex-row items-center justify-between mt-[30]' >
-                        {/* Botão Curtir */}
-                        <TouchableOpacity onPress={() => setIsLiked(!isLiked)} >
-                            <Ionicons name={isLiked ? 'heart' : 'heart-outline'} color={isLiked ? 'red' : '#767676'} size={35} />
-                        </TouchableOpacity>
-
-                        {/* Botão de Comentário */}
-                        <TouchableOpacity className='bg-[#767676] w-[280] h-[50] justify-center items-center rounded-[10px]' >
-                            <Text className='text-[18px] text-white' style={{ fontFamily: 'poppins-medium' }} >Comentários</Text>
-                        </TouchableOpacity>
+                            {/* Botão de Comentário */}
+                            <TouchableOpacity className='bg-[#767676] w-60 h-12 justify-center items-center rounded-lg' >
+                                <Text className='text-[18px] text-white' style={{ fontFamily: 'poppins-medium' }} >Comentários</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
