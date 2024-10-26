d3dd912747f585d4b5992ae1aa347e49699b35a5
Hanspeter <hansdietiker2012@gmail.com>
2024-10-26 17:19:01 -0300

    feat ScrollViews, KeyboardAvoiding <LogIn, ForgotPass,Register Screens>

--- screens/pages/ForgotPassword.tsx
+++ screens/pages/ForgotPassword.tsx
@@ -3,12 +3,16 @@ import {
   Text,
   TouchableOpacity,
   Image,
-  TextInput
+  TextInput,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  ScrollView
 } from 'react-native'
 import React from 'react'
 import { useForm, Controller } from 'react-hook-form';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 type FormData = {
   email: string
 }
@@ -17,77 +21,87 @@ export default function ForgotPassword() {
   /* Este código retorna exceção na pagina Esqueceu sua Senha */
   const { control, handleSubmit } = useForm<FormData>();
   return (
-    <View className='flex-1 bg-[#F9F9F9]  items-center' >
-      <View className='relative flex justify-center items-center  w-full h-64 mb-6'>
-        <Image
-          source={require('../../assets/icons/iconsLogin/ImagemDeFundo.png')}
-          className="absolute shadown top-0 left-0 w-full h-full bg-[#F9F9F9] object-cover"
-        />
-        <View className='justify-center items-center'>
-            <Text className='mb-4 text-4xl font-bold text-[#F9F9F9]'>ENG BOX</Text>
-            <Text className='text-[#F9F9F9] text-sm'>PENSE FORA DA CAIXA</Text>
+    <KeyboardAvoidingView
+      className='flex-1 bg-[#F9F9F9]'
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView className='bg-[#F9F9F9]'>
+
+        <View className='bg-[#F9F9F9]  items-center' >
+
+          <View className='relative flex justify-center items-center  w-full h-64 mb-6'>
+            <Image
+              source={require('../../assets/icons/iconsLogin/ImagemDeFundo.png')}
+              className="absolute shadown top-0 left-0 w-full h-full bg-[#F9F9F9] object-cover"
+            />
+            <View className='justify-center items-center'>
+              <Text className='mb-4 text-4xl font-bold text-[#F9F9F9]'>ENG BOX</Text>
+              <Text className='text-[#F9F9F9] text-sm'>PENSE FORA DA CAIXA</Text>
+            </View>
           </View>
-      </View>
-
-      {/*ForgotPassword */}
-      <View className='flex justify-center items-center'>
-        <Text style={{ fontFamily: 'poppins-semi-bold' }} className='text-[#1F3B4D] mb-4 font-bold text-3xl ml-1'>Esqueceu sua senha?</Text>
-        <Text className='text-[#455A64] ml-1 mb-8 text-justify w-[322px] font-bold  '>
-          Digite o endereço de e-mail para o qual deseja que suas informações de redefinição de senha sejam enviadas.</Text>
-      </View>
 
 
-      {/*Email*/}
-      <View className='w-4/5 mb-10'>
-        <View className='flex-row items-center mb-4 mr-5 '
-        >
-          <Ionicons name='mail' size={20} />
-          <Text className='ml-1 text-[#455A64] '>Email</Text>
+          {/*ForgotPassword */}
+          <View className='flex justify-center items-center'>
+            <Text style={{ fontFamily: 'poppins-semi-bold' }} className='text-[#1F3B4D] mb-4 font-bold text-3xl ml-1'>Esqueceu sua senha?</Text>
+            <Text className='text-[#455A64] ml-1 mb-8 text-justify w-[322px] font-bold  '>
+              Digite o endereço de e-mail para o qual deseja que suas informações de redefinição de senha sejam enviadas.</Text>
+          </View>
+
+
+          {/*Email*/}
+          <View className='w-4/5 mb-10'>
+            <View className='flex-row items-center mb-4 mr-5 '
+            >
+              <Ionicons name='mail' size={20} />
+              <Text className='ml-1 text-[#455A64] '>Email</Text>
+            </View>
+
+            <Controller
+              control={control}
+              name='email'
+              rules={{
+                required: "O Email é obrigatorio",
+                maxLength: {
+                  value: 51,
+                  message: "Limite excedido de caracteres"
+                },
+                pattern: {
+                  value: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
+                  message: 'Email inválido'
+                }
+              }}
+              render={({ field: { value, onChange }, fieldState: { error } }) => (
+                <>
+                  <TextInput
+                    className='bg-[#EDEDED] border border-[#B0BEC5] shadow rounded-2xl px-4 py-4  '
+                    placeholder='Digite seu Email'
+                    onChangeText={onChange}
+                    value={value}
+                    keyboardType='email-address'
+                    autoCapitalize='none'
+
+                  />
+                  {error && <Text
+                    style={{ fontFamily: 'poppins-semi-bold' }}
+                    className='text-[#ff375b] text-xs ml-2'>{error.message}</Text>}
+                </>
+              )}
+
+            />
+
+          </View>
+
+          {/*Button Send*/}
+          <TouchableOpacity
+            className='w-4/5 bg-[#1F3B4D] shadow-lg py-4 mb-5 rounded-2xl'
+            onPress={handleSubmit((data) => {
+              console.log(data);
+            })}>
+            <Text className='text-center text-white text-lg'>Enviar</Text>
+          </TouchableOpacity>
         </View>
-
-        <Controller
-          control={control}
-          name='email'
-          rules={{
-            required: "O Email é obrigatorio",
-            maxLength: {
-              value: 51,
-              message: "Limite excedido de caracteres"
-            },
-            pattern: {
-              value: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
-              message: 'Email inválido'
-            }
-          }}
-          render={({ field: { value, onChange }, fieldState: { error } }) => (
-            <>
-              <TextInput
-                className='bg-[#EDEDED] border border-[#B0BEC5] shadow rounded-2xl px-4 py-4  '
-                placeholder='Digite seu Email'
-                onChangeText={onChange}
-                value={value}
-                keyboardType='email-address'
-                autoCapitalize='none'
-
-              />
-              {error && <Text
-                style={{ fontFamily: 'poppins-semi-bold' }}
-                className='text-[#ff375b] text-xs ml-2'>{error.message}</Text>}
-            </>
-          )}
-
-        />
-
-      </View>
-
-      {/*Button Send*/}
-      <TouchableOpacity
-        className='w-4/5 bg-[#1F3B4D] shadow-lg py-4 mb-5 rounded-2xl'
-        onPress={handleSubmit((data) => {
-          console.log(data);
-        })}>
-        <Text className='text-center text-white text-lg'>Enviar</Text>
-      </TouchableOpacity>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView >
   )
 }
--- screens/pages/LogIn.tsx
+++ screens/pages/LogIn.tsx
@@ -7,6 +7,7 @@ import {
   KeyboardAvoidingView,
   Keyboard,
   Platform,
+  ScrollView,
 } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
@@ -32,164 +33,189 @@ export default function LogIn() {
 
   return (
 
-    <View className='flex-1 bg-[#F9F9F9] ' >
-      <View className='relative flex justify-center items-center  w-full h-64 mb-6'>
-        <Image
-          source={require('../../assets/icons/iconsLogin/ImagemDeFundo.png')}
-          className="absolute shadown top-0 left-0 w-full h-full bg-[#F9F9F9] object-cover"
-        />
-        <View className='justify-center items-center'>
-          <Text className='mb-4 text-4xl font-bold text-[#F9F9F9]'>ENG BOX</Text>
-          <Text className='text-[#F9F9F9] text-sm'>PENSE FORA DA CAIXA</Text>
-        </View>
-      </View>
-
-      <View className='flex justify-center items-center bg-[#F9F9F9]'>
-
-        {/*Wellcome*/}
-        <Text style={{ fontFamily: 'poppins-semi-bold' }} className='text-[#00796B] mb-3 font-bold text-3xl ml-2'>Bem-vindo de Volta!</Text>
-        <Text className='text-base text-[#455A64] mb-2'>Faça login na sua conta</Text>
-
-        {/*Input User*/}
-
-        <View className='w-4/5 mb-4'>
-          <View
-            className='flex-row items-center mb-2 mr-5 '
-          >
-            <Ionicons name='person-sharp' size={20} />
-
-            <Text className='ml-1 text-[#455A64]'>Usuário</Text>
-          </View>
-
-          <Controller
-            control={control}
-            name='email'
-            rules={{
-              required: "O Email ou Nome de Usuario é obrigatorio",
-              minLength: {
-                value: 3,
-                message: "Este campo deve ter no minimo 3 caracteres"
-              },
-              maxLength: {
-                value: 51,
-                message: "Limite excedido de caracteres"
-              }
-            }}
-            render={({ field: { value, onChange }, fieldState: { error } }) => (
-              <>
-                <TextInput
-                  className='bg-[#EDEDED] border border-[#B0BEC5] shadow px-4 py-4 rounded-2xl '
-                  placeholder='Nome de Usuário ou Email'
-                  value={value}
-                  onChangeText={onChange}
-                  keyboardType='email-address'
-                  autoCapitalize='none'
-                />
-                {error && <Text
-                  style={{ fontFamily: 'poppins-semi-bold' }}
-                  className='text-[#ff375b] text-xs ml-2'>{error.message}</Text>}
-              </>
-            )}
-
-          />
-
-        </View>
-
-        {/*Input Password*/}
-        <View className='w-4/5 mb-4'>
-          <View className='flex-row items-center mb-2 mr-5 '
-          >
-            <Ionicons name='lock-closed' size={20} />
-            <Text className='ml-1 text-[#455A64] '>Senha</Text>
-          </View>
-
-          <Controller
-            control={control}
-            name='password'
-            rules={{
-              required: "A senha é obrigatoria",
-              minLength: {
-                value: 3,
-                message: "A senha deve ter pelo menos 3 caracteres"
-              },
-              maxLength: {
-                value: 51,
-                message: "Limite excedido de caracteres"
-              }
-            }}
-            render={({ field: { value, onChange }, fieldState: { error } }) => (
-              <>
-                <TextInput
-                  className='bg-[#EDEDED] border border-[#B0BEC5]  shadow rounded-2xl px-4 py-4 '
-                  placeholder='Digite sua senha'
-                  value={value}
-                  onChangeText={onChange}
-                  secureTextEntry={true}
-                  autoCapitalize='none'
-                />
-                {error && <Text
-                  style={{ fontFamily: 'poppins-semi-bold' }}
-                  className='text-[#ff375b] text-xs ml-2'>{error.message}</Text>}
-              </>
-            )}
-          />
-
-        </View>
+    <KeyboardAvoidingView
+      className='flex-1 bg-[#F9F9F9]'
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
 
 
-        {/*Remember me and ForgotPassword */}
-        <View className='w-4/5 flex-row justify between mb-6'>
+      <ScrollView className='bg-[#F9F9F9]'>
+        <View className='bg-[#F9F9F9]  ' >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+            <View className='relative flex justify-center items-center  w-full h-64 mb-6'>
+              <Image
+                source={require('../../assets/icons/iconsLogin/ImagemDeFundo.png')}
+                className="absolute shadown top-0 left-0 w-full h-full bg-[#F9F9F9] object-cover"
+              />
+              <View className='justify-center items-center'>
+                <Text className='mb-4 text-4xl font-bold text-[#F9F9F9]'>ENG BOX</Text>
+                <Text className='text-[#F9F9F9] text-sm'>PENSE FORA DA CAIXA</Text>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+          <View className='flex justify-center items-center bg-[#F9F9F9]'>
 
-          <View className='flex-row items-center'>
+            {/*Wellcome*/}
+            <Text style={{ fontFamily: 'poppins-semi-bold' }} className='text-[#00796B] mb-3 font-bold text-3xl ml-2'>Bem-vindo de Volta!</Text>
+            <Text className='text-base text-[#455A64] mb-2'>Faça login na sua conta</Text>
+
+            {/*Input User*/}
+
+            <View className='w-4/5 mb-4'>
+
+              <View
+                className='flex-row items-center mb-2 mr-5 '
+              >
+
+                <Ionicons name='person-sharp' size={20} />
+
+                <Text className='ml-1 text-[#455A64]'>Usuário</Text>
+              </View>
+
+              <Controller
+                control={control}
+                name='email'
+                rules={{
+                  required: "O Email ou Nome de Usuario é obrigatorio",
+                  minLength: {
+                    value: 3,
+                    message: "Este campo deve ter no minimo 3 caracteres"
+                  },
+                  maxLength: {
+                    value: 51,
+                    message: "Limite excedido de caracteres"
+                  }
+                }}
+                render={({ field: { value, onChange }, fieldState: { error } }) => (
+
+                  <>
+
+
+                    <TextInput
+                      className='bg-[#EDEDED] border border-[#B0BEC5] shadow px-4 py-4 rounded-2xl '
+                      placeholder='Nome de Usuário ou Email'
+                      value={value}
+                      onChangeText={onChange}
+                      keyboardType='email-address'
+                      autoCapitalize='none'
+                    />
+                    {error && <Text
+                      style={{ fontFamily: 'poppins-semi-bold' }}
+                      className='text-[#ff375b] text-xs ml-2'>{error.message}</Text>}
+                  </>
+
+                )}
+
+              />
+
+            </View>
+
+
+            {/*Input Password*/}
+            <View className='w-4/5 mb-4'>
+              <View className='flex-row items-center mb-2 mr-5 '
+              >
+                <Ionicons name='lock-closed' size={20} />
+                <Text className='ml-1 text-[#455A64] '>Senha</Text>
+              </View>
+
+              <Controller
+                control={control}
+                name='password'
+                rules={{
+                  required: "A senha é obrigatoria",
+                  minLength: {
+                    value: 3,
+                    message: "A senha deve ter pelo menos 3 caracteres"
+                  },
+                  maxLength: {
+                    value: 51,
+                    message: "Limite excedido de caracteres"
+                  }
+                }}
+                render={({ field: { value, onChange }, fieldState: { error } }) => (
+
+                  <>
+
+
+
+                    <TextInput
+                      className='bg-[#EDEDED] border border-[#B0BEC5]  shadow rounded-2xl px-4 py-4 '
+                      placeholder='Digite sua senha'
+                      value={value}
+                      onChangeText={onChange}
+                      secureTextEntry={true}
+                      autoCapitalize='none'
+                    />
+                    {error && <Text
+                      style={{ fontFamily: 'poppins-semi-bold' }}
+                      className='text-[#ff375b] text-xs ml-2'>{error.message}</Text>}
+                  </>
+
+
+
+                )}
+              />
+
+            </View>
+
+
+            {/*Remember me and ForgotPassword */}
+            <View className='w-4/5 flex-row justify between mb-6'>
+
+              <View className='flex-row items-center'>
+                <TouchableOpacity
+                  className={`shadow w-6 h-6 rounded-sm border-2 ${rememberMe ? 'bg-[#455A64]' : 'bg-white border-[#D9D9D9]'}`}
+                  onPress={() => setRememberMe(!rememberMe)}
+                >
+                  {rememberMe && (
+                    <View className="w-full h-full bg-[#00796B]">
+
+                    </View>
+                  )}
+                </TouchableOpacity>
+
+                <Text
+                  className='text-gray-700 ml-2'>Lembrar de Mim</Text>
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('ForgotPassword')}>
+                  <Text
+                    className='shadow text-sm text-[#00796B] ml-6 '>Esqueceu sua Senha?</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            {/*Button Enter */}
             <TouchableOpacity
-              className={`shadow w-6 h-6 rounded-sm border-2 ${rememberMe ? 'bg-[#455A64]' : 'bg-white border-[#D9D9D9]'}`}
-              onPress={() => setRememberMe(!rememberMe)}
-            >
-              {rememberMe && (
-                <View className="w-full h-full bg-[#00796B]">
-
-                </View>
-              )}
+              className='w-4/5 bg-[#00796B] shadow-lg py-4 mb-4 rounded-2xl'
+              onPress={handleSubmit((data) => {
+                console.log(data);
+                navigation.navigate('Main');
+              })}>
+              <Text className='text-center text-white text-lg'>Entrar</Text>
             </TouchableOpacity>
 
-            <Text
-              className='text-gray-700 ml-2'>Lembrar de Mim</Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('ForgotPassword')}>
-              <Text
-                className='shadow text-sm text-[#00796B] ml-6 '>Esqueceu sua Senha?</Text>
-            </TouchableOpacity>
+            {/*Link of Register */}
+            <View className=' flex-row justify-center items-center mb-4'>
+
+              <View className='flex-row items-center'>
+                <Text
+                  className='text-gray-700 '>Não tem uma Conta?</Text>
+              </View>
+              <TouchableOpacity
+                className='shadow text-[#767676]'
+                onPress={() => navigation.navigate('Register')} >
+                <Text
+                  className='text-sm text-[#00796B] ml-1'>Registre-se</Text>
+              </TouchableOpacity>
+
+            </View>
           </View>
         </View>
 
-        {/*Button Enter */}
-        <TouchableOpacity
-          className='w-4/5 bg-[#00796B] shadow-lg py-4 mb-4 rounded-2xl'
-          onPress={handleSubmit((data) => {
-            console.log(data);
-            navigation.navigate('Main');
-          })}>
-          <Text className='text-center text-white text-lg'>Entrar</Text>
-        </TouchableOpacity>
 
-        {/*Link of Register */}
-        <View className=' flex-row justify-center items-center mb-4'>
-
-          <View className='flex-row items-center'>
-            <Text
-              className='text-gray-700 '>Não tem uma Conta?</Text>
-          </View>
-          <TouchableOpacity
-            className='shadow text-[#767676]'
-            onPress={() => navigation.navigate('Register')} >
-            <Text
-              className='text-sm text-[#00796B] ml-1'>Registre-se</Text>
-          </TouchableOpacity>
-
-        </View>
-      </View>
-    </View>
-
+      </ScrollView>
+    </KeyboardAvoidingView >
 
 
   )
