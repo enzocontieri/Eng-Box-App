645364fa017365a64db3b282e63872b8665d15a5
Hanspeter <hansdietiker2012@gmail.com>
2024-11-08 16:43:22 -0300

    feat: implemented rules at HookForm <Login

--- App.tsx
+++ App.tsx
@@ -101,7 +101,7 @@ export default function App() {
 	return (
 		<UserProvider>
 			<NavigationContainer>
-				<Stack.Navigator initialRouteName="Main">
+				<Stack.Navigator initialRouteName="Wellcome">
 					<Stack.Screen
 						name="Quiz"
 						component={Screens.Quiz}
