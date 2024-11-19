773fbadbc183f25708a7b46564f7b48e7a741ad9
Vitor <barretov585@gmail.com>
2024-11-19 16:30:36 -0300

    Sobre

--- App.tsx
+++ App.tsx
@@ -101,7 +101,7 @@ export default function App() {
 	return (
 		<UserProvider>
 			<NavigationContainer>
-				<Stack.Navigator initialRouteName="Wellcome">
+				<Stack.Navigator initialRouteName="Sobre">
 					<Stack.Screen
 						name="Quiz"
 						component={Screens.Quiz}
--- screens/options-menu/Sobre.tsx
+++ screens/options-menu/Sobre.tsx
@@ -1,56 +1,48 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
 
 const Sobre = () => {
   return (
-    <View className="mt-12" style={styles.container}>
-      <Text style={styles.title}>Sobre</Text>
-
-      <Text style={styles.subtitle}>Objetivo do aplicativo:</Text>
-      <Text style={styles.text}>
-        - Incentivar a conscientização e educação dos engenheiros sobre práticas
-        sustentáveis no dia a dia.{"\n"} - Engajar stakeholders em torno de metas de
-        sustentabilidade e redução de impacto ambiental.{"\n"} - Adotar tecnologias
-        limpas, como o uso de energia solar, eólica e biogás.{"\n"} - Desenhar produtos
-        e sistemas que utilizem menos água e emitam menos poluentes.
+    <ScrollView className="bg-[#F4F7F6] p-6">
+      <Text
+        style={{ fontFamily: "poppins-semi-bold" }}
+        className="text-[#00796B] mb-5 font-bold text-3xl shadow-sm text-center mt-5"
+      >
+        Sobre
       </Text>
 
-      <Text style={styles.subtitle}>Desenvolvedores:</Text>
-      <Text style={styles.text}>
-        - Gabriel Silva Magalhães{"\n"}- Lucas Costa Leite Santana{"\n"}- Arthur
-        Victor Vitoriano{"\n"}- Yago Mário Cerqueira{"\n"}- Edioelson Júnior
-        Teixeira{"\n"}- Enzo Colombo Contieri{"\n"}- Hanspeter Dietiker
-      </Text>
+      <View className="bg-white p-6 pt-6 pb-2 rounded-lg shadow-lg mb-5">
+        <Text className="text-[#00796B] font-bold mb-3">
+          Objetivo do aplicativo:
+        </Text>
+        <Text className="text-[#455A64] font-medium text-base mb-6">
+          - Incentivar a conscientização e educação dos engenheiros sobre
+          práticas sustentáveis no dia a dia.{"\n"}- Engajar stakeholders em
+          torno de metas de sustentabilidade e redução de impacto ambiental.
+          {"\n"}- Adotar tecnologias limpas, como o uso de energia solar, eólica
+          e biogás.{"\n"}- Desenhar produtos e sistemas que utilizem menos água
+          e emitam menos poluentes.
+        </Text>
 
-      <Text style={styles.subtitle}>Professor:</Text>
-      <Text style={styles.text}>- Welligton Lacerda</Text>
+        <Text className="text-[#00796B] font-bold mb-3">Desenvolvedores:</Text>
+        <Text className="text-[#455A64] font-medium text-base mb-6">
+          - Gabriel Silva Magalhães{"\n"}- Lucas Costa Leite Santana{"\n"}-
+          Arthur Victor Vitoriano{"\n"}- Vitor Vitoriano{"\n"}- Yago Mário
+          Cerqueira{"\n"}- Edioelson Júnior Teixeira{"\n"}- Enzo Colombo
+          Contieri{"\n"}- Hanspeter Dietiker
+        </Text>
 
-      <Text style={styles.subtitle}>Universidade:</Text>
-      <Text style={styles.text}> - UNIFACS </Text>
-    </View>
+        <Text className="text-[#00796B] font-bold mb-3">Professor:</Text>
+        <Text className="text-[#455A64] font-medium text-base mb-6">
+          - Welligton Lacerda
+        </Text>
+
+        <Text className="text-[#00796B] font-bold mb-2">Universidade:</Text>
+        <Text className="text-[#455A64] font-medium text-base">- UNIFACS</Text>
+      </View>
+    </ScrollView>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 25,
-    backgroundColor: "white",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  subtitle: {
-    fontSize: 16,
-    fontWeight: "600",
-    marginTop: 17,
-  },
-  text: {
-    fontSize: 17,
-    lineHeight: 22,
-    marginTop: 5,
-  },
-});
+
 export default Sobre;
