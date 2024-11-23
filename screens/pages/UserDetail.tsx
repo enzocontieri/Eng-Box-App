59ecaff47765d73b9888f32919f7c5f91d2ec849
enzocontieri <enzo.c.contieri@gmail.com>
2024-11-23 18:27:33 -0300

    refactor: <UserDetails> refactor header and footer, now Scrollview extends to description and image

--- screens/pages/UserDetail.tsx
+++ screens/pages/UserDetail.tsx
@@ -9,35 +9,41 @@ const UserDetail = ({ route }) => {
 
   return (
     <View className='flex-1'>
-      <View className='h-[250px] w-[100%]'>
-        <Image
-          source={CategoryIcon}
-          className='h-[250px] w-[100%] absolute top-0'
-          style={{ resizeMode: 'cover' }}
-        />
+      <View className='h-[100px] w-[100%] bg-white'>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
-          className='absolute top-10 left-5 bg-white p-2 rounded-full'
+          className='absolute top-12 left-5 bg-white p-2 rounded-full'
         >
           <Ionicons name="chevron-back" size={24} color="black" />
         </TouchableOpacity>
         <View className='items-center'>
-          <View className='absolute top-10 bg-white p-2 px-6 rounded-full'>
+          <View className='absolute top-12 bg-white p-2 px-6 rounded-full'>
             <Text className='text-[22px] text-[#767676] font-semibold' style={{ fontFamily: 'poppins-medium' }}>{title}</Text>
           </View>
         </View>
-        <View className='absolute top-10 right-5 bg-white p-2 rounded-full'>
+        <View className='absolute top-12 right-5 bg-white p-2 rounded-full'>
           <Image source={icon} className='h-6 w-6' />
         </View>
       </View>
+      <ScrollView className='bg-white' contentContainerStyle={{
+        paddingBottom: 200}}>
+        <View>
+        <Image
+            source={CategoryIcon}
+            className='h-[250px] w-[100%] top-0'
+            style={{ resizeMode: 'cover' }}
+          />
+        </View>
 
-      <View className='flex-1 mt-[-50px] bg-white rounded-t-3xl p-6'>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View className='flex flex-row justify-between items-center mb-4'>
-            <Text className='text-[14px] mt-3' style={{ fontFamily: 'poppins-medium' }}>{description}</Text>
-          </View>
-        </ScrollView>
+        <View className='flex-1 mt-[-50px] bg-white rounded-t-3xl p-6'>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View className='flex flex-row justify-between items-center mb-4'>
+              <Text className='text-[14px] mt-3' style={{ fontFamily: 'poppins-medium' }}>{description}</Text>
+            </View>
+          </ScrollView>
 
+        </View>
+    </ScrollView>
         <View className='absolute bottom-1 left-0 right-0 bg-white p-5 flex-col items-start'>
           <Text className='text-[#767676] font-normal mb-2' style={{ fontFamily: 'poppins-medium' }}>Post Criado Por</Text>
           <View className='flex-row items-center'>
@@ -51,7 +57,6 @@ const UserDetail = ({ route }) => {
             <Text className='text-[16px] font-bold' style={{ fontFamily: 'poppins-medium' }}>@{username}</Text>
           </View>
         </View>
-      </View>
     </View>
   );
 }
