033a576caa987c74b5d4974ace320a99f020dba8
Ed Araújo <ejrabt1@gmail.com>
2024-11-30 16:00:57 -0300

    test <Upload>: check operation of the 'Post Options' modal on devices

--- screens/pages/PostDetails.tsx
+++ screens/pages/PostDetails.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import * as React from 'react';
-import { Image, Modal, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Alert, Image, Modal, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const PostDetails = () => {
@@ -11,6 +11,15 @@ const PostDetails = () => {
 	const { imageUrl, titulo, conteudo } = route.params; // Parâmetros passados pela navegação
 
 	const [modalVisible, setModalVisible] = React.useState<boolean>(false);
+
+	const handleRemovePost = () => {
+		setModalVisible(false);
+		Alert.alert(
+			"Post removido",
+			"Voce removeu o post com sucesso!",
+			[{ text: "OK", onPress: () => navigation.goBack() }]
+		);
+	}
 
 	return (
 		<SafeAreaView className="flex-1">
@@ -31,18 +40,19 @@ const PostDetails = () => {
 				{/* Container Principal */}
 				<View className="flex-1 items-center justify-center mt-10">
 
-					<View className='flex self-end mr-2 mt-16' >
-						<TouchableOpacity onPress={() => {
-							setModalVisible(true);
-							console.log(modalVisible);
-						}} >
-							<Ionicons name='ellipsis-vertical' size={24} />
+					{/* Botão para abrir modal "Opcoes do Post" */}
+					<View className="flex self-end mr-2 mt-16">
+						<TouchableOpacity
+							onPress={() => setModalVisible(true)}
+							className="bg-white p-2 rounded-full"
+						>
+							<Ionicons name="ellipsis-vertical" size={24} />
 						</TouchableOpacity>
 					</View>
 
 					{/* Imagem do Post */}
 					<Image
-						source={{ uri: imageUrl}}
+						source={{ uri: imageUrl }}
 						className="w-full h-[45vh] mt-4"
 						resizeMode="cover"
 					/>
@@ -67,20 +77,18 @@ const PostDetails = () => {
 				</View>
 			</ScrollView>
 
+			{/* Modal "Opcoes do Post" */}
 			<Modal
 				visible={modalVisible}
 				transparent={true}
 				animationType='fade'
 				onRequestClose={() => setModalVisible(false)}
 			>
-				<View className='w-full h-full flex-1 items-center justify-center' >
+				<View className='w-full h-full flex-1 items-center justify-center ' >
 					<View className='bg-white w-3/4 p-6 rounded-xl shadow-md' >
 						<TouchableOpacity
 							className='flex self-end m-1'
-							onPress={() => {
-								console.log("Modal Fechado");
-								setModalVisible(false);
-							}}
+							onPress={() => setModalVisible(false)}
 						>
 							<Ionicons name='close-circle-outline' size={28} color={'#455A64'} />
 						</TouchableOpacity>
@@ -88,9 +96,7 @@ const PostDetails = () => {
 						<View className='flex items-center gap-5'>
 							<Text className='text-lg text-[#1F2B4D]' style={{ fontFamily: 'poppins-medium' }} >Opções do post</Text>
 							<TouchableOpacity
-								onPress={() => {
-									console.log("Post removido")
-								}}
+								onPress={handleRemovePost}
 								className='bg-[#455A64] items-center justify-center px-14 py-2 rounded-lg'
 							>
 								<Text className='text-base text-white' style={{ fontFamily: 'poppins-medium' }} >Remover Post</Text>
