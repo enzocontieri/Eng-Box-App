04ac541db4ae3a7ed27b67bfdde005ef2f369efa
enzocontieri <enzo.c.contieri@gmail.com>
2024-12-05 17:31:47 -0300

    feat: Create <ChangePasswordForm> component and call to <ForgotPassword>

--- screens/pages/ResetPassword.tsx
+++ screens/pages/ResetPassword.tsx
@@ -1,3 +1,9 @@
-export default function ResetPassword() {
+import ChangePasswordForm from './../../Components/forms/ChangePasswordForm';
 
+const ResetPassword = () => {
+    return (
+        <ChangePasswordForm />
+    )
 }
+
+export default ResetPassword
